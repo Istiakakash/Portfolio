6d7a63796ad49fe729e745c2a2d008de286aa415
Sarif Istiak Akash <sarif.istiak.akash@gmail.com>
2026-08-10 18:07:41 +0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const PROFILE = {
   secondaryEmail: "akash.23250407@bau.edu.bd",
   phone: "+8801676929657",
   location: "Bangladesh Agricultural University, Mymensingh-2202",
-  cvLink: "/cv2.pdf",
+  cvLink: "/cv.pdf",
   socials: {
     linkedin: "https://www.linkedin.com/in/sarif-istiak-akash/",
     googleScholar: "https://scholar.google.com/citations?user=hj1QhPoAAAAJ&hl=en",
